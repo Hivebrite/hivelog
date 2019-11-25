79c02de1d7c1698d4dbdc8305efe4096456344a7
Tian JIANG <tian@hivebrite.com>
2019-11-25 12:11:40 +0100

    Add stdout

--- lib/hivelog/logger.rb
+++ lib/hivelog/logger.rb
@@ -1,23 +1,34 @@
 require 'elasticsearch'
+require 'time'
+require 'json'
+
 module Hivelog
   class Logger
-    attr_accessor :client, :labels
+    attr_accessor :client, :labels, :output
 
     LOG_LEVELS = [:debug, :info, :warn, :error, :fatal, :panic]
+    OUTPUT = [:stdout, :elasticsearch]
     LOG_LEVELS.each do |level|
       define_method level do |*message_arg|
         create_message(level, message_arg[0], message_arg[1])
       end
     end
 
-    def initialize(url, labels = {})
+    def initialize(op, labels = {}, url = nil)
+      @output = op
+      if OUTPUT.include?(output) && output == :elasticsearch
+        @client = Elasticsearch::Client.new host: url
+      end
       @labels = labels
-      @client = Elasticsearch::Client.new host: url
     end
     
     def create_message(level, message, options = {})
       payload = build_payload(level, message, options)
-      @client.index index: generate_index, body: payload
+      if @output == :elasticsearch
+        @client.index index: generate_index(payload[:timestamp]), body: payload
+      elsif @output == :stdout
+        STDOUT.print payload.to_json
+      end
     end
 
     def build_payload(level, message, options = {})
@@ -29,8 +40,8 @@ module Hivelog
       }.merge(options)
     end
 
-    def generate_index
-      t = Time.now.utc
+    def generate_index(event_time)
+      t = event_time.utc
       "hivelog-#{t.year}.#{t.month}.#{t.day}"
     end
   end
--- spec/hivelog/logger_spec.rb
+++ spec/hivelog/logger_spec.rb
@@ -1,14 +1,17 @@
 RSpec.describe Hivelog::Logger do
   before(:context) do
-    url =  "localhost:9200"
     @labels = {
       environment: "production",
       application: "ror"
     }
-    @logger = Hivelog::Logger.new(url, @labels)
   end
-  it "has an Elasticsearch client" do
-    expect(@logger.client).not_to be nil
+
+  context "Elasticsearch output" do
+    it "has an Elasticsearch client" do
+      url =  "localhost:9200"
+      logger = Hivelog::Logger.new(:elasticsearch, @labels, url)
+      expect(logger.client).not_to be nil
+    end  
   end
 
   Hivelog::Logger::LOG_LEVELS.each do |level|
@@ -17,11 +20,8 @@ RSpec.describe Hivelog::Logger do
     end
   end
 
-  it "has an Elasticsearch client" do
-    expect(@logger.client).not_to be nil
-  end
-
   it "can build an event payload" do
+    logger = Hivelog::Logger.new(:stdout, @labels)
     event_dataset = {
       email: "foobar@hiverite.com",
       type: "sent",
@@ -49,15 +49,17 @@ RSpec.describe Hivelog::Logger do
       group: group,
       tags: tags
     }
-    payload = @logger.build_payload("debug", "toto", options)
+    payload = logger.build_payload("debug", "toto", options)
     expect(payload[:level]).to eq "debug"
     expect(payload[:message]).to eq "toto"
     expect(payload[:labels]).to eq @labels
   end
 
   it "can send a debug message" do
-    expect(@logger).to receive(:create_message).and_return(true)
-    @logger.debug("debbbbb")
+    logger = Hivelog::Logger.new(:stdout, @labels)
+    options = {}
+    expect(logger).to receive(:create_message).and_return(true)
+    logger.debug("debbbbb", options)
   end
 
 end
